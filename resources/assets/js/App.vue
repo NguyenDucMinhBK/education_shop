09b82656e3115b962589134f2439f825918ce16e
nguyenducminh <minhnd@haposoft.com>
2018-05-12 03:06:18 +0700

    test vue js with laravel done

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <transition name="fade">
+            <router-view></router-view>
+        </transition>
+    </div>
+</template>
+<script type="text/javascript">
+  export default {}
+</script>
